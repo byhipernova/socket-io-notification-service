cd45e4c79625dcd02f5cbbef901da05a11a7cdfa
Muhammet KÖKLÜ <105980019+byhipernova@users.noreply.github.com>
2023-09-20 19:46:00 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import { Server } from 'socket.io';
+import * as path from "path";
+
+const app = express();
+const server = app.listen(3000);
+const socket = new Server(server);
+const __dirname = path.resolve();
+const rooms = ['room1', 'room2', 'room3', 'room4', 'room5'];
+// for testing
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
+
+socket.on('connection', (socket) => {
+    const auth = socket.handshake.auth;
+    if(auth.token) {
+        // join room by token
+        socket.join(auth.token);
+    }
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+// send message to all clients every 300 ms
+
+setInterval(() => {
+    const room = rooms[Math.floor(Math.random() * rooms.length)];
+    socket.to(room).emit('message', {message: 'hello world', room});
+}, 300);
